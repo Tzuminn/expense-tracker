b5af5e65e56bec1594c32c0b6ba994c21b4fb7a0
Tzuminn <valuesu0326@gmail.com>
2022-11-10 04:27:33 +0800

    feat: add index and main page

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// 模板引擎
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// 靜態檔案
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
