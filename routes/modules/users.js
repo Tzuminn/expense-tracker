81b4aae8ecf58bb0cb13195bfc7ce6d575c39548
Tzuminn <valuesu0326@gmail.com>
2022-11-11 02:01:23 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,12 +6,20 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+
 // 登入功能
+router.post('/login', (req, res) => {
+})
+
 
 // 註冊頁面
-
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 // 註冊功能
 
+
 // 登出功能
+
 
 module.exports = router
